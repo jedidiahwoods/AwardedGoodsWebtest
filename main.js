a3a899cbd81065e7b20f7173468a23f4956c1130
jedidiahwoods <hi@jedidiah.life>
2025-08-08 16:09:27 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+// main.js
+// Currently, all the logic is inside index.html as an ES module.
+// This file is here as a placeholder if you want to move scripts out later.
+
+console.log("Awarded Goods 3D site loaded");
